2bccb27e8265473305e004ac044f4a90fa2af17c
Igor Tyshchenko <33253240+igoresso@users.noreply.github.com>
2024-10-16 20:44:16 +1100

    Added tooltips to the rail

--- src/lib/components/rail/rail-tile.svelte
+++ src/lib/components/rail/rail-tile.svelte
@@ -1,6 +1,8 @@
 <svelte:options runes={true} />
 
 <script lang="ts">
+	import { Button } from 'bits-ui';
+	import * as Tooltip from '$lib/components/ui/tooltip/index.js';
 	import type { Tile } from './index.ts';
 
 	type Props = {
@@ -13,24 +15,33 @@
 	let isActive = $derived(activeTile === tile.name);
 
 	function getClasses(tile: Tile) {
-		const base = 'block p-3.5 transition-colors';
-		const interactivity = `${tile.isDisabled ? 'opacity-40' : 'hover:bg-neutral-100 active:bg-neutral-300 dark:hover:bg-neutral-900 dark:active:bg-neutral-700'}`;
-		const accessibility = 'outline-none focus:ring-2 ring-ring ring-inset';
-		return `${base} ${tile.class} ${isActive ? 'bg-neutral-200 dark:bg-neutral-800 cursor-default' : interactivity} ${accessibility}`;
+		const base = 'block p-3.5 transition-colors outline-none focus:ring-2 ring-ring ring-inset';
+		const interactivity = tile.isDisabled
+			? 'opacity-40'
+			: 'hover:bg-neutral-100 active:bg-neutral-300 dark:hover:bg-neutral-900 dark:active:bg-neutral-700';
+		const activeState = isActive
+			? 'bg-neutral-200 dark:bg-neutral-800 cursor-default'
+			: interactivity;
+		return `${base} ${tile.class} ${activeState}`;
 	}
 </script>
 
-{#snippet content(tile: Tile)}
-	<tile.icon />
-	<span class="sr-only">{tile.name}</span>
-{/snippet}
-
-{#if tile.href === undefined}
-	<button class={getClasses(tile)} type="button" onclick={tile.onClick} disabled={tile.isDisabled}>
-		{@render content(tile)}
-	</button>
-{:else}
-	<a class={getClasses(tile)} href={tile.href}>
-		{@render content(tile)}
-	</a>
-{/if}
+<Tooltip.Root>
+	<Tooltip.Trigger asChild let:builder>
+		<Button.Root
+			builders={[builder]}
+			class={getClasses(tile)}
+			disabled={tile.isDisabled}
+			href={tile.href}
+			onclick={tile.onClick}
+		>
+			<tile.icon />
+			<span class="sr-only">{tile.name}</span>
+		</Button.Root>
+	</Tooltip.Trigger>
+	{#if !tile.isDisabled && !isActive}
+		<Tooltip.Content side="right">
+			{tile.name}
+		</Tooltip.Content>
+	{/if}
+</Tooltip.Root>
