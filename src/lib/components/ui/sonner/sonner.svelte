5a448c92852f8696e2a5fcc202a44c6fc42431c1
Igor Tyshchenko <33253240+igoresso@users.noreply.github.com>
2024-10-31 09:33:34 +1100

    ui/sonner update

--- src/lib/components/ui/sonner/sonner.svelte
+++ src/lib/components/ui/sonner/sonner.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
-	import { Toaster as Sonner } from 'svelte-sonner';
+	import { Toaster as Sonner, type ToasterProps as SonnerProps } from 'svelte-sonner';
 	import { mode } from 'mode-watcher';
+
+	let restProps: SonnerProps = $props();
 </script>
 
 <Sonner
@@ -8,11 +10,12 @@
 	class="toaster group"
 	toastOptions={{
 		classes: {
-			toast: 'group toast group-[.toaster]:shadow-lg',
+			toast:
+				'group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg',
 			description: 'group-[.toast]:text-muted-foreground',
 			actionButton: 'group-[.toast]:bg-primary group-[.toast]:text-primary-foreground',
 			cancelButton: 'group-[.toast]:bg-muted group-[.toast]:text-muted-foreground'
 		}
 	}}
-	{...$$restProps}
+	{...restProps}
 />
