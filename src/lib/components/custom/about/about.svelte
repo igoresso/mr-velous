b63705bbe425c7329405c3672a2b498633b283e6
Igor Tyshchenko <33253240+igoresso@users.noreply.github.com>
2025-06-22 09:36:11 +1000

    Fixed text in about

--- src/lib/components/custom/about/about.svelte
+++ src/lib/components/custom/about/about.svelte
@@ -14,7 +14,7 @@
 	</Dialog.Description>
 </Dialog.Header>
 <p class="text-balance">
-	MR Viewer with Enhanced Layout and Optimized User Experience, or simply
+	MR Viewer with Enhanced Layout and Optimized User Satisfaction, or simply
 	<span
 		class="bg-gradient-to-r from-sky-500 via-fuchsia-500 to-amber-500 bg-clip-text font-semibold text-transparent"
 	>
